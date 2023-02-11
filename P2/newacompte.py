107cf451712bd2f8cb27ad4977af6817d8cffeae
Theo PAYEN <tt.payen@gmail.com>
2023-02-11 10:02:56 +0100

    P2

---
+++ P2/newacompte.py
@@ -0,0 +1,20 @@
+import os
+
+
+# * recupere dans le fichier Fic_Nom-Groupe le non d'utilisateur et le groupe ligne par ligne
+
+# * verifie si l'utilisateur existe deja dans le fichier /etc/passwd
+
+# * verifie si le groupe existe deja dans le fichier /etc/passwd
+
+si il exite passe 
+
+si il existe pas on crée l'utilisateur
+os.system("useradd")
+os.system(" groupadd")
+
+Vérifie l’absence du user dans le fichier /etc/passwd
+
+
+Vérifie l’absence du groupe dans le fichier /etc/group avant d’exécuter depuis Python un
+groupadd
